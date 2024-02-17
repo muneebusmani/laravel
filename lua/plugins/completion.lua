ccf288b5eaea1c8a36cd2b6158ef0531a91879dc
Muneeb Usmani <muneebusmani8355@gmail.com>
2024-02-18 03:48:50 +0500

    some more changes

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,102 +1,102 @@
 return {
-  {
-    "hrsh7th/nvim-cmp",
-    version = false, -- last release is way too old
-    event = "InsertEnter",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "saadparwaiz1/cmp_luasnip",
-    },
-    opts = function()
-      vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
-      local cmp = require("cmp")
-      local defaults = require("cmp.config.default")()
-      return {
-        completion = {
-          completeopt = "menu,menuone,noinsert",
-        },
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ["<S-CR>"] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-          }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ["<C-CR>"] = function(fallback)
-            cmp.abort()
-            fallback()
-          end,
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "path" },
-          { name = "buffer" },
-          {
-            name = "html-css",
-            option = {
-              enable_on = {
-                "html",
-                "ts",
-                "js",
-              }, -- set the file types you want the plugin to work on
-              file_extensions = { "css" }, -- set the local filetypes from which you want to derive classes
-              style_sheets = {
-                "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
-              },
-            },
-          },
-        }),
-        formatting = {
-          format = function(_, item)
-            local icons = require("lazyvim.config").icons.kinds
-            if icons[item.kind] then
-              item.kind = icons[item.kind] .. item.kind
-            end
-            if _.source.name == "html-css" then
-              item.menu = _.completion_item.menu
-              return item
-            end
-            return item
-          end,
-        },
-        experimental = {
-          ghost_text = {
-            hl_group = "CmpGhostText",
-          },
-        },
-        sorting = defaults.sorting,
-      }
-    end,
-    ---@param opts cmp.ConfigSchema
-    config = function(_, opts)
-      for _, source in ipairs(opts.sources) do
-        source.group_index = source.group_index or 1
-      end
-      require("cmp").setup(opts)
-    end,
-  },
-  {
-    "Jezda1337/nvim-html-css",
-    lazy = true,
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-lua/plenary.nvim",
-    },
-    config = function()
-      require("html-css").setup()
-    end,
-  },
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   version = false, -- last release is way too old
+  --   event = "InsertEnter",
+  --   dependencies = {
+  --     "hrsh7th/cmp-nvim-lsp",
+  --     "hrsh7th/cmp-buffer",
+  --     "hrsh7th/cmp-path",
+  --     "saadparwaiz1/cmp_luasnip",
+  --   },
+  --   opts = function()
+  --     vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+  --     local cmp = require("cmp")
+  --     local defaults = require("cmp.config.default")()
+  --     return {
+  --       completion = {
+  --         completeopt = "menu,menuone,noinsert",
+  --       },
+  --       snippet = {
+  --         expand = function(args)
+  --           require("luasnip").lsp_expand(args.body)
+  --         end,
+  --       },
+  --       mapping = cmp.mapping.preset.insert({
+  --         ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+  --         ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+  --         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+  --         ["<C-f>"] = cmp.mapping.scroll_docs(4),
+  --         ["<C-Space>"] = cmp.mapping.complete(),
+  --         ["<C-e>"] = cmp.mapping.abort(),
+  --         ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  --         ["<S-CR>"] = cmp.mapping.confirm({
+  --           behavior = cmp.ConfirmBehavior.Replace,
+  --           select = true,
+  --         }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  --         ["<C-CR>"] = function(fallback)
+  --           cmp.abort()
+  --           fallback()
+  --         end,
+  --       }),
+  --       sources = cmp.config.sources({
+  --         { name = "nvim_lsp" },
+  --         { name = "luasnip" },
+  --         { name = "path" },
+  --         { name = "buffer" },
+  --         {
+  --           name = "html-css",
+  --           option = {
+  --             enable_on = {
+  --               "html",
+  --               "ts",
+  --               "js",
+  --             }, -- set the file types you want the plugin to work on
+  --             file_extensions = { "css" }, -- set the local filetypes from which you want to derive classes
+  --             style_sheets = {
+  --               "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
+  --             },
+  --           },
+  --         },
+  --       }),
+  --       formatting = {
+  --         format = function(_, item)
+  --           local icons = require("lazyvim.config").icons.kinds
+  --           if icons[item.kind] then
+  --             item.kind = icons[item.kind] .. item.kind
+  --           end
+  --           if _.source.name == "html-css" then
+  --             item.menu = _.completion_item.menu
+  --             return item
+  --           end
+  --           return item
+  --         end,
+  --       },
+  --       experimental = {
+  --         ghost_text = {
+  --           hl_group = "CmpGhostText",
+  --         },
+  --       },
+  --       sorting = defaults.sorting,
+  --     }
+  --   end,
+  --   ---@param opts cmp.ConfigSchema
+  --   config = function(_, opts)
+  --     for _, source in ipairs(opts.sources) do
+  --       source.group_index = source.group_index or 1
+  --     end
+  --     require("cmp").setup(opts)
+  --   end,
+  -- },
+  -- {
+  --   "Jezda1337/nvim-html-css",
+  --   lazy = true,
+  --   dependencies = {
+  --     "nvim-treesitter/nvim-treesitter",
+  --     "nvim-lua/plenary.nvim",
+  --   },
+  --   config = function()
+  --     require("html-css").setup()
+  --   end,
+  -- },
 }
--- lua/plugins/language-servers.lua
+++ lua/plugins/language-servers.lua
@@ -1,25 +1,25 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        dartls = {},
-        bashls = {
-          filetypes = { "sh", "zsh", "bash", "aliases", "exports" },
-        },
-        emmet_language_server = {},
-        marksman = {},
-        omnisharp = {},
-        tailwindcss = {},
-        angularls = {},
-        intelephense = {},
-        sqlls = {},
-        cssls = {},
-        jsonls = {},
-        rnix = {},
-        tsserver = {},
-        html = {},
-      },
-    },
-  },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     servers = {
+  --       dartls = {},
+  --       bashls = {
+  --         filetypes = { "sh", "zsh", "bash", "aliases", "exports" },
+  --       },
+  --       emmet_language_server = {},
+  --       marksman = {},
+  --       omnisharp = {},
+  --       tailwindcss = {},
+  --       angularls = {},
+  --       intelephense = {},
+  --       sqlls = {},
+  --       cssls = {},
+  --       jsonls = {},
+  --       rnix = {},
+  --       tsserver = {},
+  --       html = {},
+  --     },
+  --   },
+  -- },
 }
