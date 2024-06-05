05dc86a1394ec55180861ce086977cf7abfdc2ad
muneebusmani <muneebusmani8355@gmail.com>
2024-06-06 04:14:37 +0500

    updates

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -1,15 +1,14 @@
 return {
-  "vhyrro/luarocks.nvim",
-  priority = 1000,
-  config = true,
-  opts = {
-    rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" }
-  }
-},
-{
   "rest-nvim/rest.nvim",
   ft = "http",
-  dependencies = { "luarocks.nvim" },
+  dependencies = { 
+    "vhyrro/luarocks.nvim",
+    priority = 1000,
+    config = true,
+    opts = {
+      rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" }
+    },
+  },
   config = function()
     require("rest-nvim").setup()
   end,
